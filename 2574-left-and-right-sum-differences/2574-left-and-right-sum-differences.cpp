bff5388480c091b8f9a0c2bba304abeac5778eb1
Irvan Farael Hanafi <farellh12@gmail.com>
2026-08-22 17:29:57 +0700

    Time: 0 ms (100%), Space: 14.9 MB (81.3%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int total = 0;
+
+        for (int x : nums)
+            total += x;
+
+        vector<int> ans;
+        int left = 0;
+
+        for (int x : nums) {
+            int right = total - left - x;
+            ans.push_back(abs(left - right));
+            left += x;
+        }
+
+        return ans;
+    }
+};
