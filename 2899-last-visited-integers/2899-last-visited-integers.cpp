8f0a3936b049a80cdfc0f9f2c01e07907d1cc26a
Irvan Farael Hanafi <farellh12@gmail.com>
2026-08-22 17:30:39 +0700

    Time: 0 ms (100%), Space: 26 MB (39.58%) - LeetHub

---
+++ 2899-last-visited-integers/2899-last-visited-integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> lastVisitedIntegers(vector<int>& nums) {
+        vector<int> seen;
+        vector<int> ans;
+
+        int k = 0;
+
+        for (int x : nums) {
+            if (x != -1) {
+                seen.insert(seen.begin(), x);
+                k = 0;
+            } else {
+                k++;
+
+                if (k <= seen.size())
+                    ans.push_back(seen[k - 1]);
+                else
+                    ans.push_back(-1);
+            }
+        }
+
+        return ans;
+    }
+};
