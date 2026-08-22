da8d72d8a827abfa2cdeeb0c02f2eefb35324d6a
Irvan Farael Hanafi <farellh12@gmail.com>
2026-08-22 17:27:44 +0700

    Time: 0 ms (100%), Space: 20 MB (84.43%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(
+        vector<vector<int>>& items1,
+        vector<vector<int>>& items2
+    ) {
+        vector<int> weight(1001, 0);
+
+        for (auto& item : items1)
+            weight[item[0]] += item[1];
+
+        for (auto& item : items2)
+            weight[item[0]] += item[1];
+
+        vector<vector<int>> ans;
+
+        for (int value = 1; value <= 1000; value++) {
+            if (weight[value] > 0) {
+                ans.push_back({value, weight[value]});
+            }
+        }
+
+        return ans;
+    }
+};
