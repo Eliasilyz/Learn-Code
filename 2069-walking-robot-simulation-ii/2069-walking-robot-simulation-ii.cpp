aa8697b4b7a1d9515bc9fa0daf834b57510dda90
Irvan Farael Hanafi <farellh12@gmail.com>
2026-08-26 16:08:48 +0700

    Time: 50 ms (23.22%), Space: 127.4 MB (59.13%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -0,0 +1,34 @@
+// OJ: https://leetcode.com/problems/walking-robot-simulation-ii/
+// Author: github.com/lzl124631x
+// Time: 
+//      move: O(W + H)
+//      Robot, getPos, getDir: O(1)
+// Space: O(1)
+class Robot {
+    int dir = 0, dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}}, w, h, perimeter, x = 0, y = 0;
+    string text[4] = {"East","North","West","South"};
+public:
+    Robot(int width, int height) : w(width), h(height), perimeter(2*(w + h - 2)) {}
+    void step(int k) {
+        if (k >= perimeter) {
+            k %= perimeter;
+            if (x == 0 && y == 0 && dir == 0) dir = 3; // Special case: if we are at the beginning (x = 0, y = 0 and facing east), after the round trip, the direction becomes south.
+        }
+        while (k > 0) {
+            auto &[dx, dy] = dirs[dir];
+            int nx = x + dx, ny = y + dy;
+            if (nx < 0 || ny < 0 || nx >= w || ny >= h) {
+                dir = (dir + 1) % 4;
+            } else {
+                x = nx, y = ny;
+                --k;
+            }
+        }
+    }
+    vector<int> getPos() {
+        return {x,y};
+    }
+    string getDir() {
+        return text[dir];
+    }
+};
